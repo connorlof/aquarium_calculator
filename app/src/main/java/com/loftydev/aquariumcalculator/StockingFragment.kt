8aec7ee435f6889153dfd4b5ef0b01d8d28f5227
Connor Loftus <connorlof@gmail.com>
2023-07-19 09:56:00 -0400

    Styling for stocking fragment views

--- app/src/main/java/com/loftydev/aquariumcalculator/StockingFragment.kt
+++ app/src/main/java/com/loftydev/aquariumcalculator/StockingFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.core.widget.doOnTextChanged
 import com.loftydev.aquariumcalculator.databinding.FragmentStockingBinding
 import com.loftydev.aquariumcalculator.presentation.viewmodel.MenuViewModel
@@ -55,36 +56,51 @@ class StockingFragment : Fragment() {
         if (menuViewModel.lastSelectedItem.value == STOCKING_INCH_GALLON) {
             binding.tvTankDimensionHint.text = "Tank Gallons"
         } else {
-            binding.tvTankDimensionHint.text = "Tank Sq Inches (Length x Depth)"
+            binding.tvTankDimensionHint.text = "Tank Sq Inches"
         }
     }
 
     private fun observe() {
         viewModel.stockingPercent.observe(viewLifecycleOwner) { output ->
             binding.tvStockingOutput.text = formatStockingLevel(output)
+            setStockingDescription(output)
         }
     }
 
     private fun formatStockingLevel(stockingPercent: Double): String =
         try {
-            "Stocking Level: ${(stockingPercent * 100).roundToInt()}%"
+            "${(stockingPercent * 100).roundToInt()} %"
         } catch (e: Exception) {
-            "Stocking Level: 0%"
+            "0 %"
         }
 
-    private fun setListeners() {
-        binding.btnStockingCalculate.setOnClickListener { calculate() }
+    private fun setStockingDescription(stockingPercent: Double) {
+        if (stockingPercent < .85) {
+            binding.tvResultDesc.text = "Healthy"
+            binding.tvResultDesc.setTextColor(ContextCompat.getColor(requireContext(), R.color.teal_widget))
+            binding.ivTvResultInfo.tooltipText = "The amount of fish in your aquarium is at a healthy level."
+        } else if (stockingPercent in 0.85..1.0) {
+            binding.tvResultDesc.text = "Almost Full"
+            binding.tvResultDesc.setTextColor(ContextCompat.getColor(requireContext(), R.color.primary_card))
+            binding.ivTvResultInfo.tooltipText = "Your tank is almost full and it is recommended to not add new fish."
+        } else {
+            binding.tvResultDesc.text = "Overstocked"
+            binding.tvResultDesc.setTextColor(ContextCompat.getColor(requireContext(), R.color.purple_widget))
+            binding.ivTvResultInfo.tooltipText = "Your tank is overstocked and there is risk of fish health being impacted."
+        }
+    }
 
+    private fun setListeners() {
         binding.etFishInches.doOnTextChanged { _, _, _, _ ->
             MainScope().launch {
-                delay(1000)
+                delay(500)
                 calculate()
             }
         }
 
         binding.etTankDimension.doOnTextChanged { _, _, _, _ ->
             MainScope().launch {
-                delay(1000)
+                delay(500)
                 calculate()
             }
         }
