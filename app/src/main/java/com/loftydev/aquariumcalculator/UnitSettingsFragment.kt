7e9885688ea8810607ed2565dd119c3f5852dce8
Connor Loftus <connorlof@gmail.com>
2023-07-20 10:07:33 -0400

    Styling for unit settings fragment

--- app/src/main/java/com/loftydev/aquariumcalculator/UnitSettingsFragment.kt
+++ app/src/main/java/com/loftydev/aquariumcalculator/UnitSettingsFragment.kt
@@ -1,11 +1,11 @@
 package com.loftydev.aquariumcalculator
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.loftydev.aquariumcalculator.data.model.UnitSystem
 import com.loftydev.aquariumcalculator.data.model.UnitSystemType
 import com.loftydev.aquariumcalculator.databinding.FragmentUnitSettingsBinding
@@ -51,46 +51,45 @@ class UnitSettingsFragment : Fragment() {
 
     private fun setTemperatureView(unitSystemType: UnitSystemType) {
         binding.switchTemp.isChecked = unitTypeIsMetric(unitSystemType)
-        binding.switchTemp.text = unitSystemType.name
     }
 
     private fun setVolumeView(unitSystemType: UnitSystemType) {
         binding.switchVolume.isChecked = unitTypeIsMetric(unitSystemType)
-        binding.switchVolume.text = unitSystemType.name
     }
 
     private fun setDistanceView(unitSystemType: UnitSystemType) {
         binding.switchDist.isChecked = unitTypeIsMetric(unitSystemType)
-        binding.switchDist.text = unitSystemType.name
     }
 
     private fun setListeners() {
         binding.switchTemp.setOnCheckedChangeListener { _, isSelected ->
-            binding.switchTemp.text = isSelected.toUnitSystemType().name
+            binding.tvTempUnit.text = toTemperatureUnit(isSelected.toUnitSystemType())
+            saveSettings()
         }
 
         binding.switchVolume.setOnCheckedChangeListener { _, isSelected ->
-            binding.switchVolume.text = isSelected.toUnitSystemType().name
+            binding.tvVolUnit.text = toVolumeUnit(isSelected.toUnitSystemType())
+            saveSettings()
         }
 
         binding.switchDist.setOnCheckedChangeListener { _, isSelected ->
-            binding.switchDist.text = isSelected.toUnitSystemType().name
+            binding.tvDistUnit.text = toDistanceUnit(isSelected.toUnitSystemType())
+            saveSettings()
         }
+    }
 
-        binding.btnSaveSettings.setOnClickListener {
-            val selectedSettings = UnitSystem(
-                temperature = binding.switchTemp.isChecked.toUnitSystemType(),
-                volume = binding.switchVolume.isChecked.toUnitSystemType(),
-                distance = binding.switchDist.isChecked.toUnitSystemType()
-            )
+    private fun saveSettings() {
+        val selectedSettings = UnitSystem(
+            temperature = binding.switchTemp.isChecked.toUnitSystemType(),
+            volume = binding.switchVolume.isChecked.toUnitSystemType(),
+            distance = binding.switchDist.isChecked.toUnitSystemType()
+        )
 
-            // TODO: Replace with snackbar
-            viewModel.saveUnitSettings(selectedSettings).observe(viewLifecycleOwner) {
-                if (it != null) {
-                    Toast.makeText(context, "Settings saved!", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(context, "Error saving", Toast.LENGTH_SHORT).show()
-                }
+        viewModel.saveUnitSettings(selectedSettings).observe(viewLifecycleOwner) {
+            if (it != null) {
+                Log.i("Aquarium_Calculator", "Unit settings saved")
+            } else {
+                Log.e("Aquarium_Calculator", "Error saving settings")
             }
         }
     }
@@ -101,4 +100,17 @@ class UnitSettingsFragment : Fragment() {
             false -> UnitSystemType.IMPERIAL
         }
     }
+
+    private fun toTemperatureUnit(unitSystemType: UnitSystemType) =
+        if (unitSystemType == UnitSystemType.METRIC) { "Celsius" }
+        else { "Fahrenheit" }
+
+    private fun toVolumeUnit(unitSystemType: UnitSystemType) =
+        if (unitSystemType == UnitSystemType.METRIC) { "Liters" }
+        else { "Gallons" }
+
+    private fun toDistanceUnit(unitSystemType: UnitSystemType) =
+        if (unitSystemType == UnitSystemType.METRIC) { "Millimeters" }
+        else { "Inches" }
+
 }
